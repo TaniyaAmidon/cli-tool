4c3d5154400ee952f1ba6acb8dde1154f42f5a42
TaniyaAmidon <harshanitaniya@yahoo.co.uk>
2020-12-12 22:14:13 +0000

    got weather app to work 😁

--- README.md
+++ README.md
@@ -9,7 +9,24 @@ My first cli.
 [![License](https://img.shields.io/npm/l/my-cli.svg)](https://github.com/TaniyaAmidon//blob/master/package.json)
 
 <!-- toc -->
+* [Usage](#usage)
+* [Commands](#commands)
+<!-- tocstop -->
 # Usage
 <!-- usage -->
+```sh-session
+$ npm install -g my-cli
+$ my-cli COMMAND
+running command...
+$ my-cli (-v|--version|version)
+my-cli/0.0.1 darwin-x64 node-v12.1.0
+$ my-cli --help [COMMAND]
+USAGE
+  $ my-cli COMMAND
+...
+```
+<!-- usagestop -->
 # Commands
 <!-- commands -->
+
+<!-- commandsstop -->
--- src/commands/forecast.ts
+++ src/commands/forecast.ts
@@ -1,17 +1,39 @@
 import { Command, flags } from "@oclif/command";
 import * as inquirer from "inquirer";
+import axios from "axios";
 
-export class Forecast extends Command {
-  static description = "Weather forecast";
+export default class Forecast extends Command {
+  static description = "describe the command here";
 
   static flags = {
-    force: flags.boolean({ char: "f" }),
-    file: flags.string(),
+    location: flags.string(),
   };
 
+  // validations for spaces between words
+  // validation for full stops and special characters
+  // get one line weather Berlin?format=3
+  // get multiple city one line weather wttr.in/{Nuremberg,Hamburg,Berlin}?format=3'
+  // specify your own custom output format,
   async run() {
+    const url = "http://wttr.in/";
+
     const { flags } = this.parse(Forecast);
-    if (flags.force) console.log("--force is set");
-    flags.file ? console.log(`--file is: ${flags.file}`) : console.log("nope");
+
+    if (flags.location) {
+      const location = flags.location;
+      const response = await axios.get(`${url}/${location}`, {
+        headers: {
+          "User-Agent": "curl/7.54.0",
+        },
+      });
+      console.log(response.data);
+    } else {
+      const response = await axios.get(url, {
+        headers: {
+          "User-Agent": "curl/7.54.0",
+        },
+      });
+      console.log(response.data);
+    }
   }
 }
