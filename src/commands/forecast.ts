a92ddd894cbb24023f146456ea72c6ba279b2d5b
TaniyaAmidon <harshanitaniya@yahoo.co.uk>
2020-12-13 16:08:20 +0000

    basic functionality done

--- src/commands/forecast.ts
+++ src/commands/forecast.ts
@@ -3,37 +3,85 @@ import * as inquirer from "inquirer";
 import axios from "axios";
 
 export default class Forecast extends Command {
-  static description = "describe the command here";
+  static description = "get the weather forecast";
 
   static flags = {
-    location: flags.string(),
+    location: flags.boolean({
+      char: "l",
+      description: "Name of the location e.g. London",
+    }),
   };
 
-  // validations for spaces between words
-  // validation for full stops and special characters
-  // get one line weather Berlin?format=3
-  // get multiple city one line weather wttr.in/{Nuremberg,Hamburg,Berlin}?format=3'
-  // specify your own custom output format,
-  async run() {
+  callWeatherApi = async (input: string) => {
     const url = "http://wttr.in/";
+    const response = await axios.get(`${url}/${input}`, {
+      headers: {
+        "User-Agent": "curl/7.54.0",
+      },
+    });
+    console.log(response.data);
+  };
 
+  async run() {
     const { flags } = this.parse(Forecast);
+    if (!flags.location) {
+      this.callWeatherApi("");
+    }
 
     if (flags.location) {
-      const location = flags.location;
-      const response = await axios.get(`${url}/${location}`, {
-        headers: {
-          "User-Agent": "curl/7.54.0",
+      const questions = await inquirer.prompt([
+        {
+          type: "input",
+          name: "location",
+          message: "Add your location",
+          validate: (location) => {
+            if (location.length < 0) {
+              return "Add a valid location.";
+            }
+            const regex = /^[a-zA-Z\s]*$/;
+            if (location.length > 0 && !location.match(regex)) {
+              return "invalid";
+            }
+            return true;
+          },
         },
-      });
-      console.log(response.data);
-    } else {
-      const response = await axios.get(url, {
-        headers: {
-          "User-Agent": "curl/7.54.0",
+        {
+          type: "confirm",
+          name: "oneLine",
+          message: "Do you want one line format?",
+          default: false,
         },
-      });
-      console.log(response.data);
+        {
+          type: "confirm",
+          name: "multiple",
+          message: "Do you want to add more locations?",
+          default: false,
+        },
+        {
+          type: "input",
+          name: "locations",
+          message: "Add your locations.",
+          when: (answers) => answers.multiple === true,
+        },
+      ]);
+
+      if (questions.multiple) {
+        console.log("weathers", "\n");
+        if (questions.location) {
+          this.callWeatherApi(`${questions.location}?format=3`);
+        }
+
+        questions.locations.split(",").forEach((location: string) => {
+          this.callWeatherApi(`${location}?format=3`);
+        });
+      } else if (questions.oneLine) {
+        const location = `${questions.location}?format=3`;
+        console.log("\n");
+        this.callWeatherApi(location);
+      } else {
+        console.log("\n");
+        this.callWeatherApi(questions.location);
+      }
     }
   }
 }
