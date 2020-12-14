7d58b06b35eb516184ebc71dce9be249399ff38f
TaniyaAmidon <harshanitaniya@yahoo.co.uk>
2020-12-14 21:09:08 +0000

    get jokes :)

--- src/commands/jokes.ts
+++ src/commands/jokes.ts
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { Command, flags } from "@oclif/command";
 
 export default class Jokes extends Command {
@@ -5,5 +6,25 @@ export default class Jokes extends Command {
 
   static flags = {};
 
-  async run() {}
+  async run() {
+    this.getJokes();
+  }
+
+  getJokes = async () => {
+    try {
+      const JOKES_URL = "https://dad-jokes.p.rapidapi.com/random/joke";
+      const API_KEY = "8efac4fd3amshf239fb505eb2a21p12a89djsne89b3fa02711";
+
+      const response = await axios.get(JOKES_URL, {
+        headers: {
+          "x-rapidapi-key": API_KEY,
+          "x-rapidapi-host": "dad-jokes.p.rapidapi.com",
+          useQueryString: true,
+        },
+      });
+      return response.data;
+    } catch (e) {
+      console.log(e.message);
+    }
+  };
 }
