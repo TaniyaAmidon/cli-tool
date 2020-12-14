691fba158d1b55976069c994fe328c040d478ee9
TaniyaAmidon <harshanitaniya@yahoo.co.uk>
2020-12-14 22:30:04 +0000

    install node-notifier

--- src/commands/pomodoro.ts
+++ src/commands/pomodoro.ts
@@ -1,4 +1,5 @@
 import { Command, flags } from "@oclif/command";
+import notifier from "node-notifier";
 
 export default class Pomodoro extends Command {
   static description = "Set a pomodoro timer.";
