665d7bdecd7f72e5bf5e0c461c8d0ea31315ade9
TaniyaAmidon <harshanitaniya@yahoo.co.uk>
2020-12-19 20:05:47 +0000

    gen task_manager flags

--- src/commands/task_manager.ts
+++ src/commands/task_manager.ts
@@ -1,9 +1,35 @@
 import { Command, flags } from "@oclif/command";
+import fs from "fs";
 
 export default class TaskManager extends Command {
   static description = "Task manager";
 
-  static flags = {};
+  static flags = {
+    add: flags.boolean({
+      char: "a",
+      description: "Add a task.",
+    }),
+    update: flags.boolean({
+      char: "u",
+      description: "Update a task.",
+    }),
+    delete: flags.boolean({
+      char: "u",
+      description: "Delete a task.",
+    }),
+    check: flags.boolean({
+      char: "u",
+      description: "Mark as done task.",
+    }),
+  };
 
-  async run() {}
+  async run() {
+    this.writeToFile();
+  }
+
+  writeToFile = () => {
+    //fs.mkdirSync("/tmp");
+    fs.writeFileSync("/tmp/test.MD", "hello", { encoding: "utf-8" });
+    console.log("done");
+  };
 }
