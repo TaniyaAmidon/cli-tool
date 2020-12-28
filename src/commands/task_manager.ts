0a9353a2a683dfacd92d369378de0c6bb5ad7cc5
TaniyaAmidon <harshanitaniya@yahoo.co.uk>
2020-12-28 13:09:48 +0000

    write tasks to txt file

--- src/commands/task_manager.ts
+++ src/commands/task_manager.ts
@@ -1,5 +1,8 @@
 import { Command, flags } from "@oclif/command";
 import fs from "fs";
+import chalk from "chalk";
+import * as inquirer from "inquirer";
+import { Answers } from "inquirer";
 
 export default class TaskManager extends Command {
   static description = "Task manager";
@@ -9,24 +12,45 @@ export default class TaskManager extends Command {
       char: "a",
       description: "Add a task.",
     }),
+    list: flags.boolean({
+      char: "l",
+      description: "List tasks.",
+    }),
     update: flags.boolean({
       char: "u",
       description: "Update a task.",
     }),
     delete: flags.boolean({
-      char: "u",
+      char: "d",
       description: "Delete a task.",
     }),
     check: flags.boolean({
-      char: "u",
+      char: "c",
       description: "Mark as done task.",
     }),
   };
 
   async run() {
     const { flags } = this.parse(TaskManager);
+
     if (flags.add) {
-      this.writeToFile();
+      const question: Answers = await inquirer.prompt([
+        {
+          type: "input",
+          name: "addTask",
+          message: "Add a task.",
+          validate: (time: string) => {
+            if (time.length < 1) {
+              return chalk.red("uh oh! you haven't added a task yet.");
+            }
+            return true;
+          },
+        },
+      ]);
+      this.writeToFile(question.addTask);
+    }
+    if (flags.list) {
+      this.readFile();
     }
     if (flags.update) {
       // list all tasks
@@ -46,13 +70,14 @@ export default class TaskManager extends Command {
     }
   }
 
-  writeToFile = () => {
-    //fs.mkdirSync("/tmp");
-    fs.writeFileSync("test.MD", "hello", { encoding: "utf-8" });
+  writeToFile = (input: string) => {
+    //check if file is empty or not
+    fs.appendFileSync("test.txt", `- ${input}\n`, { encoding: "utf-8" });
+    console.log("Task added");
   };
 
   readFile = () => {
-    const a = fs.readFileSync("test.MD", { encoding: "utf-8" });
-    console.log(a);
+    const fileContent = fs.readFileSync("test.txt", { encoding: "utf-8" });
+    console.log(fileContent);
   };
 }
